259cf610884c31611ed828152a3d907c672e4f37
Steven Spencer <steve35spencer@gmail.com>
2020-04-27 19:11:47 -0400

    Added header/nav and general routing

--- React/saltyhacker/src/App.js
+++ React/saltyhacker/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import LoginForm from "./components/LoginForm";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router >
+      <div className="App">
+        <Header />
+        <Route exact path="/login">
+          <LoginForm />
+        </Route>
+      </div>
+    </Router>
   )
 }
 
