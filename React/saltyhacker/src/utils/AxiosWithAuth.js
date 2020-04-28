389ed926a94428fa9ca68aa375e4703f7986887f
Steven Spencer <steve35spencer@gmail.com>
2020-04-28 14:10:12 -0400

    login register flow, routing

--- React/saltyhacker/src/utils/AxiosWithAuth.js
+++ React/saltyhacker/src/utils/AxiosWithAuth.js
@@ -6,6 +6,6 @@ export const axiosWithAuth = () => {
     headers: {
       Authorization: token
     },
-    baseURL: 'http://shnt.herokuapp.com'
+    baseURL: 'https://shnt.herokuapp.com'
   });
 };
