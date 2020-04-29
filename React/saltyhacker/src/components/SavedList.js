097163eb7afdafdedb5093f157f776fa48fa71df
Ian McElroy <mcelroyian@gmail.com>
2020-04-29 11:51:10 -0500

    basic validation

--- React/saltyhacker/src/components/SavedList.js
+++ React/saltyhacker/src/components/SavedList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from '../utils/AxiosWithAuth'
+import Comment from './Comment'
 
 const SavedList = () => {
-    return(<h1>Saved List</h1>)
+
+const [savedList, setSavedList] = useState()
+
+useEffect(() => {
+    axiosWithAuth().get("/api/comments/")
+    .then(res => {
+        setSavedList([...savedList, res.data])
+        // push history to homepage 
+    })
+    .catch(err => console.log(err))
+},[])
+
+    if (savedList === undefined || savedList.length === 0) {
+        return (
+            <div>
+                <h2>Saved List</h2>
+                <p>No Saved Items.</p>
+            </div>
+            
+        )
+    }    
+
+    return(
+    <div>
+        <h1>Saved List</h1>
+        {savedList.map(saved => <Comment entry={saved} key={saved.id} />)}
+    </div>
+    
+        
+        )
 }
 
 export default SavedList;
